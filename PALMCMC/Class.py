fixed hessian

PALMCMC/Class.py
@@ -54,6 +54,7 @@ class Likelihood(object):
 		self.MLShapeCoeff = None
 		self.MeanBeta = None
 		self.MeanPhase = None
+		self.PhasePrior = None
 
 		self.doplot = None
 	
@@ -63,6 +64,8 @@ class Likelihood(object):
 		self.startPoint = None
 		self.cov_diag = None
 		self.hess = None
+		self.eigM = None
+		self.eigV = None
 
 		self.InterpolatedTime = None
 		self.InterpBasis = None
@@ -933,8 +936,11 @@ class Likelihood(object):
 	def MarginLogLike(self, x):
 	    
 
+
 		pcount = 0
 		phase=x[0]*self.ReferencePeriod#self.MeanPhase*self.ReferencePeriod
+		phasePrior = -0.5*(phase-self.MeanPhase)*(phase-self.MeanPhase)/self.PhasePrior/self.PhasePrior
+	
 		pcount += 1
 
 		NCoeff = self.MaxCoeff-1
@@ -1033,7 +1039,7 @@ class Likelihood(object):
 			    plt.plot(np.linspace(0,1,self.Nbins[i]),self.ProfileData[i]-(baseline+s[i]*amp))
 			    plt.show()
 
-		return loglike
+		return loglike+phasePrior
 
 	def calculateHessian(self,x):
 
@@ -1130,7 +1136,7 @@ class Likelihood(object):
 			PhaseScale = MLAmp/MLSigma
 
 			pcount = 0
-			HessMatrix[pcount,:] = PhaseScale*j[i]
+			HessMatrix[pcount,:] = PhaseScale*j[i]*self.ReferencePeriod
 			pcount += 1
 
 
@@ -1144,7 +1150,7 @@ class Likelihood(object):
 
 			#Hessian for Timing Model
 
-			for c in range(1, self.numTime):
+			for c in range(self.numTime):
 				HessMatrix[pcount,:] = j[i]*PhaseScale*self.designMatrix[i,c]
 				pcount += 1
 
@@ -1249,7 +1255,7 @@ class Likelihood(object):
 			PhaseScale = MLAmp/MLSigma
 
 			pcount = 0
-			HessMatrix[pcount,:] = PhaseScale*j[i]
+			HessMatrix[pcount,:] = PhaseScale*j[i]*self.ReferencePeriod
 			pcount += 1
 
 			fvals = ((self.psr.freqs[i] - self.EvoRefFreq)/1000.0)**np.arange(0,self.EvoNPoly+1)

PALMCMC/ProfileDomain.py
@@ -21,7 +21,7 @@ lfunc.loadPulsar("OneChan.par", "OneChan.tim")
 lfunc.TScrunch(doplot = False, channels = 1)
 
 
-lfunc.getInitialParams(MaxCoeff = 100, x0=[0.0, -2.0, 2.0], cov_diag=[0.1, 0.1, 1.0], resume=False)
+lfunc.getInitialParams(MaxCoeff = 100, x0=[0.0, -2.0, 2.0], cov_diag=[0.1, 0.1, 1.0], resume=True)
 
 
 
@@ -76,34 +76,28 @@ pcount += lfunc.numTime
 
 lfunc.calculateHessian(x0)
 covM=np.linalg.inv(lfunc.hess)
+lfunc.PhasePrior = np.sqrt(covM[0,0])*lfunc.ReferencePeriod
+lfunc.MeanPhase = lfunc.MeanPhase*lfunc.ReferencePeriod
 
 
-cov_diag[pcount:pcount+(lfunc.MaxCoeff-1)*(lfunc.EvoNPoly+1)] = lfunc.hess.flatten()
-
-pcount += (lfunc.MaxCoeff-1)*(lfunc.EvoNPoly+1)
-
-for i in range(lfunc.numTime):
-        cov_diag[pcount+i] = 1
-pcount += lfunc.numTime
-
-
+'''
 lfunc.calculateShapePhaseCov(x0)
 ShapePhaseCov = np.linalg.inv(lfunc.ShapePhaseCov)
 
 import scipy as sp
 SPChol = sp.linalg.cholesky(ShapePhaseCov)
 plist = [0,1,3,5,7,9,11]
+'''
 
-
-groups=[[0,1,2,3,4,5,6,7,8,9,10,11,12], [0,1,3,5,7,9,11]]
+#groups=[[0,1,2,3,4,5,6,7,8,9,10,11], [0,1,2,3,4,5,6,7]]
 lfunc.doplot=False
 burnin=1000
 sampler = ptmcmc.PTSampler(ndim=n_params,logl=lfunc.MarginLogLike,logp=lfunc.my_prior,
-                            cov=covM, outDir='./chains/',resume=False, groups=groups)
-sampler.addProposalToCycle(lfunc.TimeJump, 20)
-sampler.sample(p0=x0,Niter=100000,isave=10,burn=burnin,thin=1,neff=1000)
+                            cov=covM, outDir='./chains/',resume=False)
+#sampler.addProposalToCycle(lfunc.TimeJump, 20)
+sampler.sample(p0=x0,Niter=30000,isave=10,burn=burnin,thin=1,neff=1000)
 
-chains=np.loadtxt('./chains5/chain_1.txt').T
+chains=np.loadtxt('./chains/chain_1.txt').T
 ML=chains.T[np.argmax(chains[-3][burnin:])][:n_params]
 ML[0]=3.36203222e-03
 x0=ML