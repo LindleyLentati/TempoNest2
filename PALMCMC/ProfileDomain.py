09a227aad47c3df5ec1b17be3dfd67915d29dff8
Lindley Lentati <ltl21@login-sand3.(none)>
2016-08-05 06:28:30 +0100

    Plotting tools

--- PALMCMC/ProfileDomain.py
+++ PALMCMC/ProfileDomain.py
@@ -12,34 +12,31 @@ from Class import *
 
 
 lfunc = Likelihood()
-
-lfunc.loadPulsar("Sim.par", "OneEpoch.Noise.tim")
+lfunc.loadPulsar("OneChan.par", "OneChan.tim", root='Sim1-OneChan')
 
 
 '''Get initial Fit to the Profile'''
 
 lfunc.TScrunch(doplot = True, channels = 1)
 
-
-lfunc.getInitialParams(MaxCoeff = 2, x0=[-0.21, -1.6, np.log10(1.5)], cov_diag=[0.1, 0.1, 0.1], resume=False, outDir = './InitChains/', sampler='pal')
-
+lfunc.getInitialParams(MaxCoeff = 20, cov_diag=[0.01, 0.1, 0.1], resume=True, outDir = './InitFFTMNChains/Max20-', sampler='multinest', incScattering = False, mn_live = 1000,  fitNComps = 1, doplot = True)
 
 
 '''Make interpolation Matrix'''
 
-lfunc.PreComputeShapelets(interpTime = 1, MeanBeta = lfunc.MeanBeta)
+
+lfunc.PreComputeFFTShapelets(interpTime = 1, MeanBeta = lfunc.MeanBeta, doplot=True)
 
 lfunc.getInitialPhase(doplot = True)
-lfunc.ScatterInfo = lfunc.GetScatteringParams()
 
 
-
+lfunc.ScatterInfo = lfunc.GetScatteringParams(mode = 'parfile')
 
 parameters = []
 parameters.append('Phase')
-for i in range(lfunc.MaxCoeff-1):
+for i in range(lfunc.TotCoeff-1):
 	for j in range(lfunc.EvoNPoly+1):
-		parameters.append('S'+str(i)+'E'+str(j))
+		parameters.append('S'+str(i+1)+'E'+str(j))
 for i in range(lfunc.numTime):
 	parameters.append(lfunc.psr.pars()[i])
 for i in range(lfunc.NScatterEpochs):
@@ -54,8 +51,9 @@ lfunc.n_params = n_params
 pmin = np.array(np.ones(n_params))*-100
 pmax = np.array(np.ones(n_params))*100
 
-pmin[-1] = -5
-
+for i in range(lfunc.NScatterEpochs):
+	pmin[-lfunc.NScatterEpochs+i] = -6
+	pmax[-lfunc.NScatterEpochs+i] = 1
 
 lfunc.pmin = pmin
 lfunc.pmax = pmax
@@ -66,7 +64,7 @@ pcount = 0
 x0[pcount] = lfunc.MeanPhase
 pcount += 1
 
-for i in range(lfunc.MaxCoeff-1):
+for i in range(lfunc.TotCoeff-1):
 	for j in range(lfunc.EvoNPoly+1):
 		x0[pcount] = lfunc.MLShapeCoeff[1+i][j]
 		pcount += 1
@@ -76,51 +74,35 @@ for i in range(lfunc.numTime):
 	x0[pcount+i] = 0
 pcount += lfunc.numTime
 for i in range(lfunc.NScatterEpochs):
-	x0[pcount+i] = -2
+	x0[pcount+i] = lfunc.MeanScatter
 pcount += lfunc.NScatterEpochs
 
 
-lfunc.calculateHessian(x0)
+lfunc.calculateFFTHessian(x0)
 covM=np.linalg.inv(lfunc.hess)
-lfunc.PhasePrior = 1000*np.sqrt(covM[0,0])*lfunc.ReferencePeriod
-lfunc.MeanPhase = lfunc.MeanPhase*lfunc.ReferencePeriod
+lfunc.PhasePrior = np.sqrt(covM[0,0])*lfunc.ReferencePeriod
+lfunc.MeanPhase = x0[0]*lfunc.ReferencePeriod
 
 
-'''
-lfunc.calculateShapePhaseCov(x0)
-ShapePhaseCov = np.linalg.inv(lfunc.ShapePhaseCov)
 
-import scipy as sp
-SPChol = sp.linalg.cholesky(ShapePhaseCov)
-plist = [0,1,3,5,7,9,11]
-'''
-
-#groups=[[0,1,2,3,4,5,6,7,8,9,10,11], [0,1,2,3,4,5,6,7]]
 lfunc.doplot=False
 burnin=1000
 sampler = ptmcmc.PTSampler(ndim=n_params,logl=lfunc.FFTMarginLogLike,logp=lfunc.my_prior,
-                            cov=covM, outDir='./ScatterChain/',resume=False)
-sampler.addProposalToCycle(lfunc.TimeJump, 20)
-sampler.sample(p0=x0,Niter=30000,isave=10,burn=burnin,thin=1,neff=1000)
-'''
-chains=np.loadtxt('./FFTChains2/chain_1.txt').T
-ML=chains.T[np.argmax(chains[-3][burnin:])][:n_params]
-ML[0]=3.36203222e-03
-x0=ML
-doplot=True
-MarginLogLike(x0)
-doplot=False
+                            cov=covM, outDir='./Chains/',resume=False)
+sampler.sample(p0=x0,Niter=20000,isave=10,burn=burnin,thin=1,neff=1000)
 
-np.savetxt("ML.dat", ML)
+chains=np.loadtxt('./Chains/chain_1.txt').T
 
-chains=np.loadtxt('./FFTChains2/chain_1.txt').T
-ML=chains.T[burnin:][np.argmax(chains[-3][burnin:])][:n_params]
-STD=np.zeros(n_params)
-for i in range(n_params):
-	STD[i]  =  np.std(chains[i][burnin:])
-	print "param:", i, parameters[i], np.mean(chains[i][burnin:]), np.std(chains[i][burnin:])
-np.savetxt("Cov.dat", STD)
-cov_diag = STD
-x0=ML
 
-'''
+import corner as corner
+chains=chains[:,burnin:]
+if(lfunc.numTime > 0):
+	Tchains = chains[1+lfunc.TotCoeff-1:1+lfunc.TotCoeff-1 + lfunc.numTime]
+	figure = corner.corner(Tchains.T, labels=[r"$RA$", r"$DEC$", r"$F0$", r"$F1$"],
+		               quantiles=[0.16, 0.5, 0.84],
+		               show_titles=True, title_kwargs={"fontsize": 12})
+
+ML=chains.T[np.argmax(chains[-3])][:n_params]
+lfunc.WaterFallPlot(ML)
+
+
